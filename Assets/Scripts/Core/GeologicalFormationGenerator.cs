67a575ff0c9768dd823fefda483fd336bc910938
VastcoreDev <vastcoredev@example.com>
2025-09-04 00:58:18 +0900

    Fix VastcoreLogger namespace references and syntax errors
    - Added 'using Vastcore.Utils;' to all Core assembly files referencing VastcoreLogger
    - Fixed syntax errors in GeologicalFormationGenerator.cs and GeologicalFormationTest.cs
    - Removed duplicate using statements in VastcoreDiagnostics.cs
    
    This resolves all 89 CS0103 'VastcoreLogger does not exist' errors and syntax issues.

--- Assets/Scripts/Core/GeologicalFormationGenerator.cs
+++ Assets/Scripts/Core/GeologicalFormationGenerator.cs
@@ -1,4 +1,3 @@
-using UnityEng
 using System;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scripts/Core/VastcoreDiagnostics.cs
+++ Assets/Scripts/Core/VastcoreDiagnostics.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using Vastcore.Core;
 using Vastcore.Utils;
